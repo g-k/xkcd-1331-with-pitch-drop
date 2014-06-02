a7bae88f98c2fa583575df5d6a9cce7eb3790c03
Greg Guthe <greg.guthe@gmail.com>
2014-06-01 20:35:35 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://g-k.github.io/pitch-dropper"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2014"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [prismatic/dommy "0.1.2"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
